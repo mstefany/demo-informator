a3ece4f2be4952cf0baccdfd10ee3b2770e76236
Martin Stefany <57348587+mstefany@users.noreply.github.com>
2024-12-13 14:37:17 +0100

    fix: update to dotnet:9.0, fix env vars

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 
-var version = "0.0.1"
+var version = "0.0.1";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
@@ -18,17 +18,17 @@ app.MapGet("/info", () => new
 {
     CurrentTime = DateTime.UtcNow.ToString("o"),
     Hostname = Environment.MachineName,
-    MyNodeName = Environment.MY_NODE_NAME,
-    MyPodName = Environment.MY_POD_NAME,
-    MyPodNamespace = Environment.MY_POD_NAMESPACE,
-    MyPodIp = Environment.MY_POD_IP,
-    MyPodServiceAccount = Environment.MY_POD_SERVICE_ACCOUNT
+    MyNodeName = Environment.GetEnvironmentVariable("MY_NODE_NAME"),
+    MyPodName = Environment.GetEnvironmentVariable("MY_POD_NAME"),
+    MyPodNamespace = Environment.GetEnvironmentVariable("MY_POD_NAMESPACE"),
+    MyPodIp = Environment.GetEnvironmentVariable("MY_POD_IP"),
+    MyPodServiceAccount = Environment.GetEnvironmentVariable("MY_POD_SERVICE_ACCOUNT")
 });
 
 // Version endpoint
 app.MapGet("/version", () => new
 {
-    AppVersion = $version
+    AppVersion = version
 });
 
 app.Run("http://*:8080");
