5d2eab11dc1b04edbf15936cc736609bbc5c61d8
Martin Stefany <57348587+mstefany@users.noreply.github.com>
2024-12-13 19:06:28 +0100

    fix: ubi8 workdirs

--- src/Program.cs
+++ src/Program.cs
@@ -5,22 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 
-var version = "0.0.1";
+var version = "0.0.3";
 var builder = WebApplication.CreateBuilder(args);
 
-// Get port from environment variable or use default
-var port = Environment.GetEnvironmentVariable("LISTEN_PORT");
-if (!int.TryParse(port, out int parsedPort))
-{
-    parsedPort = 8080; // Default to 8080 if not set or invalid
-}
-
-if (parsedPort < 1024)
-{
-    Console.WriteLine("Error: Port number must be 1024 or higher.");
-    return;
-}
-// Add services to the container
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -43,4 +30,4 @@ app.MapGet("/version", () => new
     AppVersion = version
 });
 
-app.Run($"http://*:{parsedPort}");
+app.Run($"http://*:8080");
